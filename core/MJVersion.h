e6503a223b9ddf597a1bc6b85cbdc65f159c4168
Dave Frampton <davidframpton@majicjungle.com>
2026-04-23 08:39:56 +1200

    version bump

--- core/MJVersion.h
+++ core/MJVersion.h
@@ -2,7 +2,7 @@
 #ifndef __Version__
 #define __Version__
 
-#define KATIPO_VERSION "0.2.1.0" //must not contain spaces
+#define KATIPO_VERSION "0.2.2.0" //must not contain spaces
 //todo #define NETWORK_COMPATIBILITY_VERSION 1 //increment to ensure clients have up to date version, stored in uint16_t
 
 #endif
